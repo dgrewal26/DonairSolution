91bd3522b972be44b9667115486a6ce7e45b0ec4
dgrewal26 <dsingh26@studentmail.nait.ca>
2021-02-03 14:19:45 -0700

    Update DonairHubDBSQLQuery.sql

--- DonairHubDBSQLQuery.sql
+++ DonairHubDBSQLQuery.sql
@@ -9,14 +9,18 @@ GO
 Drop Table if exists Users
 Drop Table if Exists Role
 
+Drop Table if Exists OrderedItems
+
+Drop Table if Exists Items
+Drop Table if Exists Categories
 
 
-
+Drop Table if Exists Orders
 
 ----Role table
 CREATE TABLE Role
 (
-	ID int Identity(1,1) Constraint PK_Role Primary Key NOT NULL,
+	ID int Identity(1,1) Primary Key NOT NULL,
 	RoleName varchar(16) NOT NULL,
 	Description varchar(60) NOT NULL
 )
@@ -28,18 +32,64 @@ GO
 ---User table
 CREATE TABLE Users
 (
-	ID INT IDENTITY(1,1) Constraint PK_Users  Primary Key NOT NULL,
+	ID INT IDENTITY(1,1) Primary Key NOT NULL,
 	FirstName nvarchar(60) NOT NULL,
 	LastName nvarchar(60) NOT NULL,
 	Email nvarchar(60) NOT NULL,
 	Password text NOT NULL,
-	RoleID INT,
-	Constraint FK_Role Foreign Key (RoleID) References Role(ID)
+	RoleID INT Foreign Key (RoleID) References Role(ID)
 )
 --ALTER TABLE Users
 	--ADD CONSTRAINT PK_Users PRIMARY KEY(ID),
 		--CONSTRAINT FK_Role FOREIGN KEY(RoleID) REFERENCES Role(ID)
 GO
+
+--Categories Table
+Create Table Categories
+(
+	ID INT IDENTITY(1,1) Primary Key NOT NULL,
+	Name nVarchar(50) NOT NULL,
+	Description nVarchar(100) NOT NULL,
+	IsDeleted bit  Check(IsDeleted = 'Y' OR IsDeleted = 'N')
+	 Default('N')
+)
+
+--Items Table
+Create Table Items
+(
+	ID INT IDENTITY(1,1) Primary Key NOT NULL,
+	CategoryID INT  Foreign Key(CategoryID) References Categories(ID),
+	Name nVarchar(50) NOT NULL,
+	Description nVarchar(100) NOT NULL,
+	Image Varbinary(Max) null,
+	UnitPrice Money not null,
+	IsDeleted bit Check(IsDeleted = 'Y' OR IsDeleted = 'N')
+	 Default('N')
+
+)
+
+
+--Orders Table
+Create Table Orders
+(
+	ID INT IDENTITY(1,1)  Primary Key NOT NULL,
+	Date nVarchar(30) not null,
+	CustomerName nVarchar(50) not null,
+	Phone nVarchar(20) not null,
+	Email nVarchar(50) null,
+	OrderStatus nVarchar(50) not null
+)
+
+
+--OrderedItems Table
+Create Table OrderedItems
+(
+	ID INT IDENTITY(1,1)  Primary Key NOT NULL,
+	ItemID INT  Foreign Key(ItemID) References Items(ID),
+	OrderID INT  Foreign Key(OrderID) References Orders(ID),
+	Quantity INT not null,
+	SpecialRequest nVarchar null
+)
 
 --drop procedure CreateUser
 
